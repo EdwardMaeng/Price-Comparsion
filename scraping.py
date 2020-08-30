c2eaf20c86f0bd214d6d8db7ce328b3d45329ab9
EdwardMaeng <70357468+EdwardMaeng@users.noreply.github.com>
2020-08-29 23:47:33 -0700

    Modified scraping/searching

--- scraping.py
+++ scraping.py
@@ -1,8 +1,8 @@
-import requests
+from searching import giveURL
+import requests, time
 from bs4 import BeautifulSoup
 
-
-URL = 'https://www.amazon.com/Blink-Mini-Indoor-Camera/dp/B07X6C9RMF/ref=sr_1_3?dchild=1&keywords=camera&qid=1598758469&sr=8-3'
+URL = giveURL()
 
 headers = {
     "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
@@ -12,11 +12,20 @@ page = requests.get(URL, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-product_name = soup.find(id="productTitle").get_text()
-product_price = soup.find(id="priceblock_ourprice").get_text()
+containers = soup.findAll("span", {"class":"a-price-whole"})
+time.sleep(3)
+print(containers)
+
+
+# for container in containers:
+#     brand = containers.div.div.a.img["title"]
+#     print("brand: " + brand)
+
+#product_name = soup.find(id="productTitle").get_text()
+#product_price = soup.find(id="priceblock_ourprice").get_text()
 #product_imagelink = soup.find(id="imgTageWrapperID").get_text()
 
-converted_price = float(product_price[1:6])
+#converted_price = float(product_price[1:6])
 
-print(product_name.strip())
-print(converted_price)
+#print(product_name.strip())
+#print(converted_price)
